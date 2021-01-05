b8c2fc0e29a614cf2bceec50215015aabeb5a151
0GiS0 <gisela.torres@returngis.net>
2021-01-05 16:04:56 +0100

    delete node_modules

--- script.sh
+++ script.sh
@@ -29,7 +29,7 @@ kubectl get nodes
 echo "# aks-devops-demo" >> README.md
 git init
 git add .
-git commit -m "first commit"
+git commit -m "added script and gitignore"
 git branch -M main
 git remote add origin https://github.com/0GiS0/aks-devops-demo.git
 git push -u origin main
