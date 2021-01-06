e72624dc764eea9102a475ad708fe9839a6641ff
0GiS0 <gisela.torres@returngis.net>
2021-01-06 22:01:13 +0100

    changed response again

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('Testing DevOps with Azure DevOps');
+	res.send('Testing DevOps with Azure DevOps with AKS');
 });
 
 app.listen(port, () => {
